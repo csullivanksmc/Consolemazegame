a4cc18b955e49d62d7668bba8e414a28f1c209e6
Chris Sullivan <csullivan@KSMConsulting.com>
2017-04-06 06:18:50 -0400

    first working version of maze mechanics

---
+++ ConsolePlatformer/PartyPosition.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePlatformer
+{
+    public class PartyPosition
+    {
+        public void drawMe()
+        {
+            
+            Console.SetCursorPosition(this.xPos, this.yPos);
+            Console.Write("P");
+
+
+        }
+
+        public PartyPosition(int xpos, int ypos)
+        {
+            this.xPos = xpos;
+            this.yPos = ypos;
+
+        }
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        
+        public void MoveUp()
+        {
+            Console.SetCursorPosition(this.xPos, this.yPos);
+            Console.Write(" ");
+            this.yPos--;
+        }
+
+        public void MoveDown()
+        {
+            Console.SetCursorPosition(this.xPos, this.yPos);
+            Console.Write(" ");
+            this.yPos++;
+        }
+
+        public void MoveRight()
+        {
+            Console.SetCursorPosition(this.xPos, this.yPos);
+            Console.Write(" ");
+            this.xPos++;
+        }
+
+        public void MoveLeft()
+        {
+            Console.SetCursorPosition(this.xPos, this.yPos);
+            Console.Write(" ");
+            this.xPos--;
+        }
+
+    }
+}
---
+++ ConsolePlatformer/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePlatformer
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            PartyPosition Location = new PartyPosition(1, 1);
+
+            string maze = @"+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+|                       |                   |                       |           |
++---+   +---+   +---+---+   +---+   +---+---+   +---+   +---+---+   +   +---+   +
+|       |   |   |           |   |           |       |   |   |       |       |   |
++   +---+   +   +   +---+---+   +---+---+   +   +---+   +   +   +---+---+---+   +
+|       |       |   |                   |       |       |       |               |
++   +   +   +---+   +---+   +   +---+   +---+---+   +---+---+   +---+   +---+   +
+|   |   |   |   |       |   |   |       |       |   |       |           |       |
++---+   +   +   +---+   +---+   +   +---+---+   +   +   +   +---+---+---+   +---+
+|       |       |       |       |               |       |   |       |       |   |
++   +   +---+---+   +---+   +---+---+---+---+   +---+---+   +---+   +   +---+   +
+|   |   |       |   |           |           |   |       |       |   |   |       |
++   +---+   +   +   +---+---+   +---+   +   +   +   +   +---+   +   +   +   +   +
+|   |       |       |       |       |   |   |   |   |       |   |   |       |   |
++   +   +---+---+---+   +   +---+   +   +   +   +---+---+   +   +   +---+---+   +
+|   |   |               |           |   |   |               |   |               |
++   +   +---+---+---+   +---+---+---+   +   +---+---+---+   +   +---+---+   +---+
+|       |               |   |           |           |       |   |       |       |
++   +---+   +---+---+---+   +   +---+---+---+---+   +   +---+   +   +   +---+   +
+|   |       |           |   |   |       |       |   |   |   |       |   |       |
++   +   +   +   +---+   +   +   +   +   +   +   +   +   +   +---+---+   +   +---+
+|       |   |       |   |           |   |   |   |   |   |           |   |       |
++   +---+---+---+   +   +---+---+---+   +   +   +   +   +   +---+   +   +---+   +
+|   |               |           |   |       |   |           |   |   |       |   |
++---+   +---+---+---+---+---+   +   +---+   +---+---+---+---+   +   +---+   +   +
+|   |   |       |           |   |       |   |           |       |       |   |   |
++   +   +   +---+   +---+   +   +---+   +   +   +---+   +---+   +---+   +   +   +
+|   |   |           |       |       |       |   |           |           |   |   |
++   +   +   +---+---+   +---+---+   +   +---+   +---+---+   +---+---+   +   +---+
+|   |   |   |   |       |           |       |       |   |           |   |       |
++   +   +   +   +   +---+   +---+---+---+---+---+   +   +---+---+   +   +---+   +
+|       |   |   |           |                       |               |       |   |
++---+---+   +   +---+---+---+---+   +   +---+---+---+   +---+---+---+---+   +   +
+|       |       |               |   |       |       |           |           |   |
++   +   +---+   +---+---+   +   +   +---+   +   +   +---+---+   +---+---+---+   +
+|   |       |       |       |   |       |   |   |   |       |           |       |
++   +   +---+---+   +   +---+   +   +---+   +---+   +   +   +---+---+   +   +---+
+|   |           |   |   |       |   |       |       |   |           |   |       |
++   +---+   +---+   +   +   +---+---+   +---+   +---+   +---+---+   +   +---+   +
+|       |               |               |                       |               |
++---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+";
+
+            Console.WriteLine(maze);
+
+            Console.WriteLine(maze[166]);
+            Console.WriteLine(maze[Location.xPos + ((Location.yPos) * 83)]);
+
+            while (true)
+            { 
+
+                if (Console.KeyAvailable)
+                { 
+
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+                    if (keyInfo.Key == ConsoleKey.UpArrow && maze[(Location.xPos + ((Location.yPos) * 83) - 83)].ToString() != "-" && maze[(Location.xPos + ((Location.yPos) * 83) - 83)].ToString() != "+" )
+                    {
+                        Location.MoveUp();
+                        
+                    }
+                    else if (keyInfo.Key == ConsoleKey.DownArrow && maze[(Location.xPos + ((Location.yPos) * 83) + 83)].ToString() != "-" && maze[(Location.xPos + ((Location.yPos) * 83) + 83)].ToString() != "+")
+                    {
+                        Location.MoveDown();
+                        
+                    }
+                    else if (keyInfo.Key == ConsoleKey.LeftArrow && maze[(Location.xPos + ((Location.yPos) * 83) - 1)].ToString() != "|" && maze[(Location.xPos + ((Location.yPos) * 83) - 1)].ToString() != "+")
+                    {
+                        Location.MoveLeft();
+                        
+                    }
+                    else if (keyInfo.Key == ConsoleKey.RightArrow && maze[(Location.xPos+((Location.yPos)*83)+1)].ToString() != "|" && maze[(Location.xPos + ((Location.yPos) * 83) + 1)].ToString() != "+")
+                    {
+                        Location.MoveRight();
+                        
+                    }
+
+                }
+
+                Location.drawMe();
+                Console.SetCursorPosition(Location.xPos, Location.yPos);
+                
+                
+            }
+        }
+    }
+}
